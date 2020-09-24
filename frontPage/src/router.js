f178a71e9976c05669b833252480815a5ef323c9
yangqing <401219180@qq.com>
2020-09-24 14:00:23 +0800

    更新

--- frontPage/src/router.js
+++ frontPage/src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/UIlist.vue';
+// import Home from './views/UIlist.vue';
 // import wyc from './views/wyc.vue';
 
 Vue.use(Router);
@@ -10,7 +10,7 @@ export default new Router({
     {
       path: '/',
       name: '主页',
-      component: () => import('./views/UIlist.vue')
+      component: () => import('./views/home.vue')
     },
     {
       path: '/ps',
---
+++ frontPage/src/views/home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="home" :style="background">
+      <img src="../assets/logo.png" alt="Vue logos">
+    </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  data () {
+    return {
+      background: {
+        backgroundImage: 'url(' + require('../assets/background.jpg') + ')',
+        backgroundRepeat: "no-repeat",
+        // backgroundSize: "100% 100%",
+      },
+    }
+  },
+  components: {
+  }
+}
+</script>
